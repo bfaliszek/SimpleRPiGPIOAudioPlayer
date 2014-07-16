ed5e95b13ebc507a5207b420f6e710980146c6a4
Blazej Faliszek <blazejfaliszek@gmail.com>
2014-07-16 15:19:55 +0200

    General Update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 SimpleRPiGPIOAudioPlayer
 ========================
+
+Add `@reboot sleep 10 && /home/pi/autorun && sleep 2 && echo -n p > /tmp/cmd &` to the end of `sudo crontab -e`
+
+Copy "player.py" and "autorun" to the pi's home directory(/home/pi/).
--- player.py
+++ player.py
@@ -19,41 +19,45 @@ os.system('/usr/local/bin/gpio mode 7 out')
 os.system('/usr/local/bin/gpio write 7 1')
 print 'Laduje pliki audio...\n'
 os.system('/usr/bin/omxplayershuffle local /media/*/ < /tmp/cmd &')
+
 #print 'Play >\n'
 #os.system('echo -n p > /tmp/cmd &')
 
 x=1
+#y=0
 
 while True:
+#    if (y==0):
+#        os.system('echo -n p > /tmp/cmd &')
+#        y=1
     if ( GPIO.input(23) == False ):
         if(x==1):
             os.system('echo -n p > /tmp/cmd &')
             print 'Play >\n'
-            sleep(0.2);
+            sleep(0.5);
         else:
-            os.system('echo -n q > /tmp/cmd &')
             os.system('omxplayershuffle local /media/*/ < /tmp/cmd &')
             os.system('echo -n p > /tmp/cmd &')
             print 'Pauza ||\n'
             x=1
-            sleep(0.2);
+            sleep(0.5);
     if ( GPIO.input(24) == False ):
-        os.system('echo -n q > /tmp/cmd &')
+        os.system('sudo pkill omxplayer')
         os.system('omxplayershuffle local /media/*/ < /tmp/cmd &')
         os.system('echo -n p > /tmp/cmd &')
         print 'Shuffle\n'
         x=1
-        sleep(0.2);
+        sleep(0.5);
     if ( GPIO.input(25)== False ):
         os.system('sudo pkill omxplayer')
         print 'Stop\n'
-        x=x-1
-        sleep(0.2);
+        x=0
+        sleep(0.5);
     if ( GPIO.input(22) == False ):
         os.system('echo -n i > /tmp/cmd &')
         print '<< Poprzednia\n'
-        sleep(0.2);
+        sleep(0.5);
     if ( GPIO.input(10) == False ):
         os.system('echo -n o > /tmp/cmd &')
         print 'Nastepna >>\n'
-        sleep(0.2);
+        sleep(0.5);
